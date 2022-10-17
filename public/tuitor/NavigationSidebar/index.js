f28a980bb787a8db29c06ef4601432df3f3055c5
Kaushik Komanduri <komanduri.k@northeastern.edu>
2022-10-16 22:45:27 -0400

    new changes

--- public/tuitor/NavigationSidebar/index.js
+++ public/tuitor/NavigationSidebar/index.js
@@ -2,16 +2,16 @@ const NavigationSidebar = () => {
     return(`
     <ul class="list-group wd-unique">
       <li class="list-group-item wd-main-menu"><i class="fab fa-twitter" style="font-size:30px"></i></li>
-      <li class="list-group-item wd-main-menu">
+      <li class="list-group-item wd-main-menu wd-highlight-home">
         <div class="row">
           <div class="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2 col-xxl-2"><i class="fa-solid fa-house"></i></div>
-          <div class="d-none d-xxl-block d-xl-block col-4 col-sm-0 col-md-0 col-lg-0 col-xl-4 col-xxl-4"><a href="../HomeScreen/explore.html"">Home</a></div>
+          <div class="d-none d-xxl-block d-xl-block col-4 col-sm-0 col-md-0 col-lg-0 col-xl-4 col-xxl-4"><a href="../HomeScreen/explore.html" id="wd-anchor">Home</a></div>
         </div>
       </li>
-      <li class="list-group-item wd-main-menu">
+      <li class="list-group-item wd-main-menu wd-highlight-explore">
         <div class="row">
           <div class="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2 col-xxl-2"><i class="fa-solid fa-hashtag"></i></div>
-          <div class="d-none d-xxl-block d-xl-block col-4 col-sm-0 col-md-0 col-lg-0 col-xl-4 col-xxl-4"><a href="../explore/explore.html">Explore</a></div>
+          <div class="d-none d-xxl-block d-xl-block col-4 col-sm-0 col-md-0 col-lg-0 col-xl-4 col-xxl-4"><a href="../explore/explore.html" id="wd-anchor">Explore</a></div>
         </div>
       </li>
       <li class="list-group-item wd-main-menu">
