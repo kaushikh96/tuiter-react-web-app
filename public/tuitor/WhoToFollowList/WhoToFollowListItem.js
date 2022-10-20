5915612326cda8e5c1b1267ff04832433ae5be6d
Kaushik Komanduri <komanduri.k@northeastern.edu>
2022-10-20 13:18:43 -0400

    alignment changes

--- public/tuitor/WhoToFollowList/WhoToFollowListItem.js
+++ public/tuitor/WhoToFollowList/WhoToFollowListItem.js
@@ -2,18 +2,18 @@ const WhoToFollowListItem = (who) => {
     return(`            
             <li class="list-group-item wd-follow-label">
                 <div class="row wd-row">
-                    <div class="col-8">
+                    <div class="col-9">
                       <div class="d-flex flex-row align-items-center">
                         <img
                         class="wd-tweet-pages-pic"
                         src="${who.avatarIcon}"
                       />
                       <div>
-                        <b>${who.userName}</b>&nbsp;<i class="fa fa-check-circle"></i><br/><span class="wd-handler">@${who.handle}</span>
+                        <b>${who.userName}</b>&nbsp;<i class="fa fa-check-circle"></i><div class="wd-handler">@${who.handle}</div>
                       </div>
                     </div>
                       </div>
-                    <div class="col-4">
+                    <div class="col-3">
                         <button
                     id="wd-follow-button"
                     type="submit"
